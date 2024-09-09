a4b6923f4e2561850ac48b3e486480985858dead
Your Name <you@example.com>
2024-09-09 15:36:45 +0200

    dfgh

--- Assets/Scripts/BoltProperties.cs
+++ Assets/Scripts/BoltProperties.cs
@@ -5,11 +5,14 @@ public class BoltProperties : NetworkBehaviour {
     [Networked] public NetworkBool IsGold { get; set; }
     [Networked] public float ConveyorSpeed { get; set; }
     [Networked] public NetworkBool OnConveyor { get; set; }
+    [Networked] public NetworkBool IsGrabbed { get; set; }
 
     private MeshRenderer meshRenderer;
+    private Rigidbody rb;
 
     public override void Spawned() {
         meshRenderer = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
         UpdateVisuals();
     }
 
@@ -20,11 +23,23 @@ public class BoltProperties : NetworkBehaviour {
     }
 
     public override void FixedUpdateNetwork() {
-        if (OnConveyor) {
-            this.transform.Translate(Vector3.right * ConveyorSpeed * Runner.DeltaTime);
+        if (OnConveyor && !IsGrabbed) {
+            transform.Translate(Vector3.right * ConveyorSpeed * Runner.DeltaTime);
+        }
+
+        if (Object.HasStateAuthority) {
+            // Sync physics properties
+            rb.position = transform.position;
+            rb.rotation = transform.rotation;
         }
     }
 
+    public void grab() {
+        RPC_GrabBolt(Runner.LocalPlayer);
+    }
+    public void release() {
+        RPC_ReleaseBolt(new Vector3(0,0,0));
+    }
     public override void Render() {
         UpdateVisuals();
     }
@@ -35,60 +50,37 @@ public class BoltProperties : NetworkBehaviour {
         }
     }
 
-    public void grab() {
-        RPC_GrabBolt();
-    }
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void RPC_GrabBolt(PlayerRef player) {
+        if (Object.HasStateAuthority) {
+            IsGrabbed = true;
+            Object.AssignInputAuthority(player);
+            rb.isKinematic = true;
 
-    public void release() {
-        RPC_ReleaseBolt();
-    }
-
-    [Rpc(RpcSources.All, RpcTargets.All)]
-    public void RPC_GrabBolt() {
-
-        Debug.Log("Grabbed any bolt!");
-        // Debug.Log(this.GetComponent<Fusion.XR.Shared.Grabbing.NetworkGrabbable>().IsGrabbed);
-
-        // Debug.Log(this.GetComponent<NetworkHandColliderGrabbable>().);
-        GetComponent<NetworkObject>().AssignInputAuthority(Runner.LocalPlayer);
-        GetComponent<NetworkObject>().RequestStateAuthority();
-        Debug.Log(Runner.LocalPlayer);
-        Debug.Log(GetComponent<NetworkObject>().HasInputAuthority);
-        Debug.Log(GetComponent<NetworkObject>().InputAuthority);
-        Debug.Log(GetComponent<NetworkObject>().HasStateAuthority);
-        Debug.Log(GetComponent<NetworkObject>().StateAuthority);
-
-
-
-
-        if (!IsGold) {
-            // Handle error logic here
-            Debug.Log("Grabbed silver bolt - Error!");
-            // You might want to call a method on a manager object to handle the error
-            // For example: GameManager.Instance.MadeError();
+            if (!IsGold) {
+                Debug.Log("Grabbed silver bolt - Error!");
+                // Handle error logic here
+            }
         }
-        //Runner.Despawn(Object);
     }
 
-    [Rpc(RpcSources.All, RpcTargets.All)]
-    public void RPC_ReleaseBolt() {
-
-        Debug.Log("released any bolt!");
-        // Debug.Log(this.GetComponent<Fusion.XR.Shared.Grabbing.NetworkGrabbable>().IsGrabbed);
-
-        // Debug.Log(this.GetComponent<NetworkHandColliderGrabbable>().);
-        //<NetworkObject>().AssignInputAuthority(Runner.LocalPlayer);
-        GetComponent<NetworkObject>().ReleaseStateAuthoirty();
-
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_ReleaseBolt(Vector3 releaseVelocity) {
+        if (Object.HasStateAuthority) {
+            IsGrabbed = false;
+           // Object.InputAuthority = PlayerRef.None;
+            rb.isKinematic = false;
+            rb.velocity = releaseVelocity;
+        }
     }
 
-    public  void OnCollisionEnter(Collision collision) {
+    public void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Conveyor")) {
             OnConveyor = true;
         }
     }
 
-    public  void OnCollisionExit(Collision collision) {
+    public void OnCollisionExit(Collision collision) {
         if (collision.gameObject.CompareTag("Conveyor")) {
             OnConveyor = false;
         }
